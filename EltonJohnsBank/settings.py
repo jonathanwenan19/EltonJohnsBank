73ef143896af5cc479021f10528e618ad76f4025
jonathanwenan19 <71362203+jonathanwenan19@users.noreply.github.com>
2022-06-12 22:03:03 +0700

    Add files via upload

--- EltonJohnsBank/settings.py
+++ EltonJohnsBank/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@pmt473v@gazy-+%##jgxq6q!&6vg=15@%7kf(^8*qb*11h627'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['eltonjohnsbank.herokuapp.com','127.0.0.1']
 
@@ -148,7 +148,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 STATIC_URL = '/static/'
 
