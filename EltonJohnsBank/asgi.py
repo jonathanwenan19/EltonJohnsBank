b5621526c1aadc0b913f9afe0a592d374c3e478c
jonathanwenan19 <71362203+jonathanwenan19@users.noreply.github.com>
2022-06-12 21:21:20 +0700

    Add files via upload

---
+++ EltonJohnsBank/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for EltonJohnsBank project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EltonJohnsBank.settings')
+
+application = get_asgi_application()
